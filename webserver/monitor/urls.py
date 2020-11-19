b0a1198daebcc25ecd42940d3b2994d20a63a88a
luhuadong <luhuadong@163.com>
2020-11-19 10:39:21 +0800

    update monitor urls

--- webserver/monitor/urls.py
+++ webserver/monitor/urls.py
@@ -13,19 +13,19 @@ urlpatterns = [
     url(r'^dologin$', views.dologin, name="dologin"),
     url(r'^logout$',  views.logout,  name="logout"),
 
-    url(r'^monitor/$', views.index, name='index'),
-    url(r'^monitor/(?P<pIndex>[0-99]+)$', views.index, name='index'),
+    url(r'^', views.index, name='index'),
+    url(r'(?P<pIndex>[0-99]+)$', views.index, name='index'),
 
-    url(r'^monitor/add$', views.add, name='monitor_add'),
-    url(r'^monitor/insert$', views.insert, name='monitor_insert'),
-    url(r'^monitor/delete/(?P<hid>\d+)$', views.delete, name='monitor_delete'),
-    url(r'^monitor/edit/(?P<hid>\d+)$', views.edit, name='monitor_edit'),
-    url(r'^monitor/update/(?P<hid>\d+)$', views.update, name='monitor_update'),
+    url(r'^add$', views.add, name='monitor_add'),
+    url(r'^insert$', views.insert, name='monitor_insert'),
+    url(r'^delete/(?P<hid>\d+)$', views.delete, name='monitor_delete'),
+    url(r'^edit/(?P<hid>\d+)$', views.edit, name='monitor_edit'),
+    url(r'^update/(?P<hid>\d+)$', views.update, name='monitor_update'),
 
     #url(r'^monitor/(?P<pIndex>[0-99]+)$', views.index, name='index'),
     #url(r'^monitor/(?P<pIndex>[0-99]+)$', views.index, name='index'),
 
-    url(r'^monitor/details/(?P<hid>\d+)$', views.details, name='details'),
-    url(r'^monitor/manual', views.manual, name='manual'),
-    url(r'^monitor/settings', views.settings, name='settings'),
+    url(r'^details/(?P<hid>\d+)$', views.details, name='details'),
+    url(r'^manual', views.manual, name='manual'),
+    url(r'^settings', views.settings, name='settings'),
 ]
